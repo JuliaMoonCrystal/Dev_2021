5346c8e7026835479b6aa4492fa4ae5d7b8d6dad
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-08 19:18:32 -0300

    .

--- HTML-JS-CSS/formulario_pro_js.js
+++ HTML-JS-CSS/formulario_pro_js.js
@@ -3,7 +3,12 @@ var btEnviar=function(){
     alert('Botão foi usado');
 }
 
+var btlIMPAR=function(){
+   console.log("Botão limpar foi usado");
+}
+
 var btAux=document.getElementById('enviar');
 btAux.onclick=btEnviar;
 
 var bt=document.getElementById("Limpar");
+bt.onclick=btlIMPAR;
