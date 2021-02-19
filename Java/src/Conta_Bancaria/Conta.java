15a68b3b0342f64a2009f902916002c7015e5db2
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-18 21:00:56 -0300

    .

---
+++ Java/src/Conta_Bancaria/Conta.java
@@ -0,0 +1,23 @@
+package Conta_Bancaria;
+
+public class Conta {
+  private String nome;
+  private int numConta;
+  private double saldo;
+  
+  
+  
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public int getNumConta() {
+	return numConta;
+}
+public void setNumConta(int numConta) {
+	this.numConta = numConta;
+}
+  
+}
---
+++ Java/src/Conta_Bancaria/operacoes.java
@@ -0,0 +1,5 @@
+package Conta_Bancaria;
+
+public class operacoes {
+
+}
