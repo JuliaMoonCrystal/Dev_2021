7449e75abce7b9f881f9e93c5a74759b6305d1ea
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-22 20:59:15 -0300

    .

---
+++ Java/src/JavaFX/Login.java
@@ -0,0 +1,13 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
