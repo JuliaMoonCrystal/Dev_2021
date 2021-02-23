7ca409205dd3be26c9a06d6d03af366fe3dc35a4
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-22 21:12:16 -0300

    .

--- Java/src/JavaFX/Login.java
+++ Java/src/JavaFX/Login.java
@@ -1,13 +1,34 @@
 package JavaFX;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Login extends Application{
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage stage) throws Exception {
+		AnchorPane pane= new AnchorPane();
+		pane.setPrefSize(500,500);
 		
+		
+		TextField txtLogin= new TextField();
+		txtLogin.setPromptText("Digite a sua senha");
+		
+		PasswordField txtSenha= new PasswordField();
+		txtSenha.setPromptText("Digite a sua senha");
+		
+		Scene cena= new Scene(pane);
+	
+		stage.setScene(cena);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
