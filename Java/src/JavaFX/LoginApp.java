bccaf710e2927ae37600dd39b3f5c16cc85c2272
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-25 20:16:02 -0300

    .

--- Java/src/JavaFX/LoginApp.java
+++ Java/src/JavaFX/LoginApp.java
@@ -1,6 +1,8 @@
 package JavaFX;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -12,7 +14,7 @@ public class LoginApp extends Application{
 	private AnchorPane pane;
 	private TextField txtLogin;
 	private PasswordField txtSenha;
-	private Button btnLogin;
+	private Button btnLogin,btnSair;
 	private static Stage stage;
 	
 	
@@ -39,6 +41,28 @@ public class LoginApp extends Application{
 		pane= new AnchorPane();
 		pane.setPrefSize(400, 300);
 		pane.setStyle("-fx--background-color: linear-gradient(from 0% 0% to 100% 100% blue 0%, silver 100%);");
+		
+		txtLogin= new TextField();
+		txtLogin.setPromptText("Digite a sua senha");
 	}
-
+	
+	private void initLayout(){
+		txtLogin.setLayoutX((pane.getWidth()-txtLogin.getHeight())/2);
+		txtLogin.setLayoutY(50);
+	}
+	
+	private void initListener() {
+	 btnSair.setOnAction(new EventHandler<ActionEvent>() {
+		
+		@Override
+		public void handle(ActionEvent event) {
+           fechaApp();			
+		}
+	});	 
+}
+	
+	private void fechaApp() {
+        System.exit(0);
+		
+	}
 }
