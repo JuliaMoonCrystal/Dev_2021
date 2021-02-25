a23767bb2a3176fa7f19762cc36c7f9921e73ec8
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-25 20:02:29 -0300

    .

---
+++ Java/src/JavaFX/LoginApp.java
@@ -0,0 +1,19 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginApp extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
