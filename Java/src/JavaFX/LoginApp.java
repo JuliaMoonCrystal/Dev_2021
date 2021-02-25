2e1e69fc19454daa614dda29cfc5ac589bfe2439
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-25 20:09:10 -0300

    ;

--- Java/src/JavaFX/LoginApp.java
+++ Java/src/JavaFX/LoginApp.java
@@ -1,10 +1,24 @@
 package JavaFX;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class LoginApp extends Application{
 
+	private AnchorPane pane;
+	private TextField txtLogin;
+	private PasswordField txtSenha;
+	private Button btnLogin;
+	private static Stage stage;
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -12,8 +26,19 @@ public class LoginApp extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+
 		
+		LoginApp.stage=stage;
+	}
+	
+	public static Stage getStage() {
+		return stage;
+	}
+	
+	private void initComponent() {
+		pane= new AnchorPane();
+		pane.setPrefSize(400, 300);
+		pane.setStyle("-fx--background-color: linear-gradient(from 0% 0% to 100% 100% blue 0%, silver 100%);");
 	}
 
 }
