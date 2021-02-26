1ce207b3d35e2395dc10be78d160385290c7ffd8
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-25 22:04:31 -0300

    .

---
+++ Java/src/JavaFX/Carrinho.java
@@ -0,0 +1,20 @@
+package JavaFX;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho {
+  private static List<Produto> produtos= new ArrayList<Produto>();
+  
+  public void addProdutos(Produto... ps) {
+	  for(Produto p : ps) {
+		  produtos.add(p);
+	  }
+  }
+  
+  public List<Produto> getProdutos(){
+	  return produtos;
+  }
+  
+  
+}
--- Java/src/JavaFX/Produto.java
+++ Java/src/JavaFX/Produto.java
@@ -5,6 +5,25 @@ public class Produto {
    private double preco;
    
    public Produto(String produto,double preco) {
-	   
+	   this.setProduto(produto);
+	   this.setPreco(preco);
    }
+
+public String getProduto() {
+	return produto;
+}
+
+public void setProduto(String produto) {
+	this.produto = produto;
+}
+
+public double getPreco() {
+	return preco;
+}
+
+public void setPreco(double preco) {
+	this.preco = preco;
+}
+   
+   
 }
