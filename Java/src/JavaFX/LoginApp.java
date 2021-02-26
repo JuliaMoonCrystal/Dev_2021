f0c28420ea4ca51368d7112f1d4d4007e5adda8b
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-25 21:16:21 -0300

    .

--- Java/src/JavaFX/LoginApp.java
+++ Java/src/JavaFX/LoginApp.java
@@ -22,7 +22,7 @@ public class LoginApp extends Application{
 
 	@Override
 	public void start(Stage Stage) throws Exception {
-      
+        
 		AnchorPane pane= new AnchorPane();
 		pane.setPrefSize(400, 300);
 		pane.setStyle("-fx--background-color: linear-gradient(from 0% 0% to 100% 100% blue 0%, silver 100%);");
@@ -37,10 +37,9 @@ public class LoginApp extends Application{
 		Button btnLogin= new Button("Logar");
 		Button btnSair= new Button("Sair");
 		
+		// definição do Layout dos campos e posições
 		txtLogin.setLayoutX(50);
 		txtLogin.setLayoutY(50);
-		
-		
 		
 		txtSenha.setLayoutX(50);
 		txtSenha.setLayoutY(100);
@@ -48,13 +47,42 @@ public class LoginApp extends Application{
 		btnLogin.setLayoutX(50);
 		btnLogin.setLayoutY(150);
 		
+		btnSair.setLayoutX(100);
+		btnSair.setLayoutY(150);
+		
+		//add na tela
 		pane.getChildren().addAll(txtLogin,txtSenha,btnLogin,btnSair);
 		
-		 Scene scene = new Scene(pane);
-	        Stage.setScene(scene);
-	        Stage.setResizable(false);
-	        Stage.setTitle("Login");
-	        Stage.show();
+		Scene scene = new Scene(pane);
+        Stage.setScene(scene);
+        Stage.setResizable(false);
+        Stage.setTitle("Login");
+        Stage.show();
+        
+        //ações dos botões
+		btnLogin.setOnAction(new EventHandler<ActionEvent>() {
+        
+			public void handle(ActionEvent event) {
+               logar();
+				
+			}
+		});
+		
+		btnSair.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+               fecharApp();				
+			}
+		});
 	}
-	
+
+	protected void logar() {
+		  System.out.println("Botão clicado");
+			}
+		
+	protected void fecharApp() {
+      System.exit(0);	
+	}
+
 }
