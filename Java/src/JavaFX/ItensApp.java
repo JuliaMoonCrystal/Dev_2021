3b008e58645c0d245edb53b631424433d3974adb
Julia Farias da Rocha <jufarias20002@gmail.com>
2021-02-26 21:14:11 -0300

    .

--- Java/src/JavaFX/ItensApp.java
+++ Java/src/JavaFX/ItensApp.java
@@ -1,15 +1,39 @@
 package JavaFX;
 
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
+
 import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import sun.launcher.resources.launcher;
 
 public class ItensApp extends Application {
 
-	@Override
+	
 	public void start(Stage Stage) throws Exception {
-
-		
+       
+		// elementos
+	   AnchorPane pane;
+       ImageView imgitem;
+       Label lbPreco,lbDescricao;
+       Stage stage;
+       Produto produto;
+       int index;
+       
+       pane= new AnchorPane();
+       pane.setPrefSize(600, 400);
+	
+       lbPreco=new Label();
+       lbDescricao= new Label();
+       
+       imgitem= new ImageView(new Image("Captura de Tela(333).png"));
+       imgitem.setLayoutX(10);
+       imgitem.setLayoutY(0);
+      
+       
+       
 	}
 
 	
