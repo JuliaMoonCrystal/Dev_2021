d7d950c7d7d1bca1f263030eebdd13c4f8f25cb4
Julia F <jufarias20002@gmail.com>
2021-03-07 19:25:07 -0300

    .

--- BD_SQL/academia.sql
+++ BD_SQL/academia.sql
@@ -10,5 +10,14 @@ nome VARCHAR(100)
 CREATE TABLE atividade(
 codigo INT NOT NULL ,
 descricao VARCHAR(100),
-imc DECIMAL
+imc DECIMAL(7,2)
 )
+
+INSERT INTO atividade VALUEs
+(1,'Corrida + Step', 18.5),
+(2,'Biceps + Costas + Pernas', 24.9),
+(3,'Esteira+ Biceps + Costas + Pernas', 29.9),
+(4,'Bicicleta + Biceps + Costas + Pernas', 34.9),
+(5,'Esteira + Bicicleta ',39.9)
+
+SELECT*FROM atividade
