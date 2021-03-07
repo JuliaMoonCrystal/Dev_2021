5b287bc3a54208f0f629eab8ac503e6d872f0ec3
Julia F <jufarias20002@gmail.com>
2021-03-07 19:36:43 -0300

    .

--- BD_SQL/academia.sql
+++ BD_SQL/academia.sql
@@ -3,14 +3,25 @@ GO
 USE academia
 
 CREATE TABLE alunos(
-codigo_aluno INT,
+codigo_aluno INT PRIMARY KEY,
 nome VARCHAR(100)
 )
 
 CREATE TABLE atividade(
-codigo INT NOT NULL ,
+codigo INT NOT NULL PRIMARY KEY,
 descricao VARCHAR(100),
 imc DECIMAL(7,2)
+)
+
+CREATE TABLE Atividade_Alunos(
+Aluno_Codigo INT ,
+altura DECIMAL(7,2),
+peso DECIMAL(7,2),
+imc DECIMAL(7,2),
+atividade_codigo INT
+PRIMARY KEY(Aluno_codigo,atividade_codigo),
+FOREIGN  key(Aluno_codigo) REFERENCES alunos(codigo_aluno),
+FOREIGN  key(atividade_codigo) REFERENCES atividade(codigo)
 )
 
 INSERT INTO atividade VALUEs
@@ -21,3 +32,9 @@ INSERT INTO atividade VALUEs
 (5,'Esteira + Bicicleta ',39.9)
 
 SELECT*FROM atividade
+SELECT*FROM alunos
+SELECT*FROM Atividade_Alunos
+
+--DROP TABLE atividade
+--DROP TABLE alunos
+--DROP TABLE Atividade_Alunos
