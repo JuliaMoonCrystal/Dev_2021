1d3bf007224f44de5820932f3128a0da70c4066c
Julia F <jufarias20002@gmail.com>
2021-03-14 22:17:20 -0300

    .

--- HTML-JS-CSS/e-commerce/form_feed.js
+++ HTML-JS-CSS/e-commerce/form_feed.js
@@ -1,9 +1,13 @@
 function enviar(){
   var nome=document.getElementById('nome');
+  var email=document.getElementById('email');
+  
 
-  if(nome.value !=""){
+  if(nome.value !="" && email.value !=""){
       alert('Obrigado pela sua opinião');
   }
-
+  else{
+      alert('Digite alguma coisa pra que possamos registrar o seu comentário')
+  }
 
 }
