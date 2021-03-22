bbcad599289e0d4aa7139369f552ff5dff6dd476
Julia F <jufarias20002@gmail.com>
2021-03-22 20:51:53 -0300

    .

--- Imersão_Dev_Alura/Js_Aula_01.js
+++ Imersão_Dev_Alura/Js_Aula_01.js
@@ -1,6 +1,13 @@
 var BotaoClicado=function(){
     alert("Função funcionando");
+    var valorLibra=document.getElementById("Libra");
+   
+
+    alert("Aqui está"+valorLibra);
+
+
  }
 
  var BotaoAdi= document.getElementById("converter");
  BotaoAdi.onclick=BotaoClicado;
+
