63437fda44ba6e5d6be8fbb54a37f22ebb0a471f
Julia F <jufarias20002@gmail.com>
2021-03-22 21:09:31 -0300

    .

--- Imersão_Dev_Alura/Js_Aula_01.js
+++ Imersão_Dev_Alura/Js_Aula_01.js
@@ -1,6 +1,7 @@
 var BotaoClicado=function(){
-    alert("Função funcionando");
-    var valorLibra=document.getElementById("Libra");
+    var valorDolar =1.39;
+     
+    var Libra=parseFloat(document.getElementById("Libra"));
    
     prompt("Digite o valor em libras");
 
