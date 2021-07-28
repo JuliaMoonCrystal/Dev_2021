6a62c72fd3eb4120b125d6d02c21c89bacd7e4ce
julia <jufarias20002@gmail.com>
2021-07-28 18:11:11 -0300

    .

--- GFT START #2 Java/Testes com java/CalculadoraTest.java
+++ GFT START #2 Java/Testes com java/CalculadoraTest.java
@@ -3,6 +3,7 @@ package com.teste;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
 
 public class CalculadoraTest {
 
@@ -10,8 +11,18 @@ public class CalculadoraTest {
 	public void test() {
 		
 		Calculadora calc= new Calculadora();
-		int soma= calc.somar("1+1+3");
-		assertEquals(5, soma);
+		int sum= calc.somar("1+1+3");
+		assertEquals(5, sum);
+	}
+	
+	@Test
+	public void testeSomarComMock(){
+		Calculadora calculadora= mock(Calculadora.class);
+		
+		when(calculadora.somar("1+2").thenReturn(10));
+		int resultado = calculadora.somar("1+2");
+		
+		assertEquals(10, resultado);
 	}
 
 }
