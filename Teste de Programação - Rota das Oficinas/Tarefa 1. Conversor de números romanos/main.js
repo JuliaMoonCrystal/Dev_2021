034e79e36d47398ba6e8ed075ce55f3be5106926
Julia <jufarias20002@gmail.com>
2021-07-31 20:36:47 -0300

    .

--- Teste de Programação - Rota das Oficinas/Tarefa 1. Conversor de números romanos/main.js
+++ Teste de Programação - Rota das Oficinas/Tarefa 1. Conversor de números romanos/main.js
@@ -1,12 +1,12 @@
 
 function Inverter(){
     document.getElementById("romano").innerHTML="<b>Digite o número</b>";
-    document.getElementById("convertido").innerHTML="<b>Número Romano</b>";
+    document.getElementById("convertido").innerHTML="<b>Número Romano:</b>";
 }
 
 function Converter(){
     var romano=document.getElementById("romano");
-    var ro=romano.value;
+    var aux=romano.value;
     
 }
   var MatrizRomana=[[1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],[100, 'C'],[90, 'XC'],[50, 'L'],[40, 'XL'],[10, 'X'],[9, 'IX'],
@@ -16,3 +16,14 @@ function Converter(){
 
   /*Testando para ver se a matriz foi populada*/
   console.log(MatrizRomana);
+
+  function convertToRoman(num) {
+    if (num === 0) {
+      return '';
+    }
+    for (var i = 0; i < romanMatrix.length; i++) {
+      if (num >= romanMatrix[i][0]) {
+        return romanMatrix[i][1] + convertToRoman(num - romanMatrix[i][0]);
+      }
+    }
+  }
