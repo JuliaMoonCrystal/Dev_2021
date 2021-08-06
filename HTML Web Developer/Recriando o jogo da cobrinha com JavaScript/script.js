8513d8f70431ab4f207c6f5fb560acdf04a4d4c5
julia <jufarias20002@gmail.com>
2021-08-06 12:40:04 -0300

    criando movimento

--- HTML Web Developer/Recriando o jogo da cobrinha com JavaScript/script.js
+++ HTML Web Developer/Recriando o jogo da cobrinha com JavaScript/script.js
@@ -1,3 +1,7 @@
+
+/*
+ Variaveis
+ */
 let canvas =document.getElementById("cobra");
 let context =canvas.getContext("2d");
 let box= 32;
@@ -8,12 +12,16 @@ cobra[0]={
     y: 8*box
 }
 
+let direcao="right";
+let jogo= setInterval(IniciarJogo, 100);
+
+
+/*Funções*/
 function criarBG(){
     context.fillStyle="yellow";
     context.fillRect(0,0,16 * box, 16*box);
 }
 
-criarBG();
 
 function CriarCobrinha(){
     for (let i=0; i<cobra.length; i++){
@@ -22,4 +30,26 @@ function CriarCobrinha(){
     }
 }
 
-CriarCobrinha();
+function IniciarJogo(){
+    criarBG();
+    CriarCobrinha();
+
+    let cobrax= cobra[0].x;
+    let cobray= cobra[0].y;
+
+    if(direcao == "right") cobrax+= box;
+    if(direcao == "left") cobrax-= box;
+    if(direcao == "up") cobray-= box;
+    if(direcao == "down") cobray+= box;
+
+    cobra.pop();
+
+    let newHead={
+        x: cobrax,
+        y: cobray
+    }
+
+    cobra.unshift(newHead);
+    
+}
+
