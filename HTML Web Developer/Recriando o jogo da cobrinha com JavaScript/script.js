6bb2e9dea66af10913e358b6f3b122e41b60c4e2
julia <jufarias20002@gmail.com>
2021-08-06 12:11:03 -0300

    criando a cobrinha

--- HTML Web Developer/Recriando o jogo da cobrinha com JavaScript/script.js
+++ HTML Web Developer/Recriando o jogo da cobrinha com JavaScript/script.js
@@ -1,6 +1,12 @@
 let canvas =document.getElementById("cobra");
 let context =canvas.getContext("2d");
 let box= 32;
+let cobra=[];
+
+cobra[0]={
+    x: 8*box,
+    y: 8*box
+}
 
 function criarBG(){
     context.fillStyle="yellow";
@@ -8,3 +14,12 @@ function criarBG(){
 }
 
 criarBG();
+
+function CriarCobrinha(){
+    for (let i=0; i<cobra.length; i++){
+        context.fillStyle="black";
+        context.fillRect(cobra[i].x,cobra[i].y, box,box);
+    }
+}
+
+CriarCobrinha();
