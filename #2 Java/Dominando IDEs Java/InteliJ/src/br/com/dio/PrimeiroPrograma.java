9d1dccf627e4eedb8e7aea9de90e8657b1e0c400
Julia <jufarias20002@gmail.com>
2021-08-12 20:40:56 -0300

    commit

--- #2 Java/Dominando IDEs Java/InteliJ/src/br/com/dio/PrimeiroPrograma.java
+++ #2 Java/Dominando IDEs Java/InteliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -8,10 +8,46 @@ public class PrimeiroPrograma {
         Gato gato= new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("O guia do mochileiro das galáxias",250);
+        System.out.println(livro);
         /*int a=10;
         int b= 17;
 
         System.out.println("Hello world  "+ (a+b));*/
 
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
