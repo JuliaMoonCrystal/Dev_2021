2719c2fc5bfe32d98d2fbc389b4f27d3fe5d9d3f
Julia <jufarias20002@gmail.com>
2021-08-12 20:06:33 -0300

    .

--- #2 Java/Dominando IDEs Java/InteliJ/src/br/com/dio/PrimeiroPrograma.java
+++ #2 Java/Dominando IDEs Java/InteliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -1,5 +1,12 @@
 package br.com.dio;
 
 public class PrimeiroPrograma {
-    
+    public static void main(String[] args) {
+
+        /*int a=10;
+        int b= 17;
+
+        System.out.println("Hello world  "+ (a+b));*/
+        
+    }
 }
