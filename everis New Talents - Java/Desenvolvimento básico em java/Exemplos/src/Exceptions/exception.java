5660a7559bf4144f562d86c97231c2dfcb2eecfb
julia <jufarias20002@gmail.com>
2021-08-14 10:09:07 -0300

    .

--- everis New Talents - Java/Desenvolvimento básico em java/Exemplos/src/Exceptions/exception.java
+++ everis New Talents - Java/Desenvolvimento básico em java/Exemplos/src/Exceptions/exception.java
@@ -1,11 +1,25 @@
 
 package Exceptions;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class exception {
 
 	public static void main(String[] args) {
 
 		metodo();
+		metodo2();
+	}
+
+	private static void metodo2() {
+try {
+	Object con;
+	Object query;
+	PreparedStatement stmt= con.prepareStatement(query);
+} catch (SQLException e) {
+    throw new Exception("Problema na criação do Statment", e);
+   }		
 	}
 
 	private static void metodo() {
