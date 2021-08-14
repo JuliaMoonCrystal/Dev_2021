42c2bc727491c001209b3e9e9ccbfafeba709116
Julia <jufarias20002@gmail.com>
2021-08-13 21:15:54 -0300

    .

---
+++ everis New Talents - Java/Desenvolvimento básico em java/Exemplos/src/Calendar/Exemplo005.java
@@ -0,0 +1,13 @@
+package Calendar;
+
+import java.util.Calendar;
+
+public class Exemplo005 {
+    public static void main(String[] args) {
+        Calendar calendario=  Calendar.getInstance();
+
+        System.out.println(calendario);
+
+
+    }
+}
