5ab06a9f9da560124e71300326614741b01d6169
Julia <jufarias20002@gmail.com>
2021-08-14 19:08:36 -0300

    .

--- everis New Talents - Java/Desenvolvimento básico em java/Exemplos/src/Exceptions/CarroVo.java
+++ everis New Talents - Java/Desenvolvimento básico em java/Exemplos/src/Exceptions/CarroVo.java
@@ -4,23 +4,31 @@ public class CarroVo {
    private String marca;
    private String cor;
    private String placa;
+
    
+
 public String getMarca() {
+
 	return marca;
 }
-public void setMarca(String marca) {
+public void setMarca(String marca)
+{
 	this.marca = marca;
 }
-public String getCor() {
+public String getCor()
+{
 	return cor;
 }
-public void setCor(String cor) {
+public void setCor(String cor)
+{
 	this.cor = cor;
 }
-public String getPlaca() {
+public String getPlaca()
+{
 	return placa;
 }
 public void setPlaca(String placa) {
+
 	this.placa = placa;
 }
    
