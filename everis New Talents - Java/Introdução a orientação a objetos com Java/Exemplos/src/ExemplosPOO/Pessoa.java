0564584d33f10b5a9b3fa410d6279d82daec2e2a
Julia <jufarias20002@gmail.com>
2021-08-18 20:37:07 -0300

    .

--- everis New Talents - Java/Introdução a orientação a objetos com Java/Exemplos/src/ExemplosPOO/Pessoa.java
+++ everis New Talents - Java/Introdução a orientação a objetos com Java/Exemplos/src/ExemplosPOO/Pessoa.java
@@ -1,6 +1,9 @@
 package ExemplosPOO;
-
+/*Toda a classe extends Object*/
 public class Pessoa {
+
+    private  static  final int Tamanho_cpf=11;
+    private  static  final int Tamanho_cnpj=12;
 
     public enum TipoPessoa{FISICA, JURIDICA};
     public Integer codigo;
@@ -8,4 +11,25 @@ public class Pessoa {
     public String documento;
     public TipoPessoa tipo;
 
+    public  String getDocumento(){
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        if(documento == null || documento.isEmpty()){
+             throw new RuntimeException("Documento não pode ser vazio ou nulo");
+        }
+        if(documento.length() == Tamanho_cpf){
+            tipo=TipoPessoa.FISICA;
+        } else if (documento.length() == Tamanho_cnpj){
+            tipo= TipoPessoa.JURIDICA;
+        }else {
+            throw  new RuntimeException("Tamanho do CPF ou Cnpj inválidos");
+        }
+        this.documento = documento;
+    }
+
+    private void setDocumento(String documento, TipoPessoa tipo) {
+
+    }
 }
