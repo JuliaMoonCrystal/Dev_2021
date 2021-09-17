40640f3d0ce7bf79bbcb367b9dde08e4fd498daa
Julia <jufarias20002@gmail.com>
2021-09-17 00:25:44 -0300

    Estudos sobre JavaFx

---
+++ JavaFX/Fx/src/Telas/Login.java
@@ -0,0 +1,19 @@
+package Telas;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		
+	}
+
+}
