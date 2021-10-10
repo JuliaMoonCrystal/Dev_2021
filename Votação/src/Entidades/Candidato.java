f97156db5c3bca83392d9af3395ab53d0ecf60fa
Julia <jufarias20002@gmail.com>
2021-10-10 11:21:46 -0300

    entidades - finalizadas

---
+++ Votação/src/Entidades/Candidato.java
@@ -0,0 +1,32 @@
+package Entidades;
+
+public class Candidato {
+	private String nome;
+	private int numCandidato;
+	private String nomePartido;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNumCandidato() {
+		return numCandidato;
+	}
+
+	public void setNumCandidato(int numCandidato) {
+		this.numCandidato = numCandidato;
+	}
+
+	public String getNomePartido() {
+		return nomePartido;
+	}
+
+	public void setNomePartido(String nomePartido) {
+		this.nomePartido = nomePartido;
+	}
+
+}
---
+++ Votação/src/Entidades/Votacao.java
@@ -0,0 +1,20 @@
+package Entidades;
+
+public class Votacao {
+ private int numeroSecao;
+
+ 
+ public Votacao(){
+	 
+ }
+ 
+public int getNumeroSecao() {
+	return numeroSecao;
+}
+
+public void setNumeroSecao(int numeroSecao) {
+	this.numeroSecao = numeroSecao;
+}
+ 
+ 
+}
