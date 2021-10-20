d761a8dddb129f9ca0bb2766bcfe55d45885779a
Julia <jufarias20002@gmail.com>
2021-10-19 22:36:05 -0300

    Introdução ao Typescript

--- Eduzz Fullstack Developer/Introdução ao TypeScript/Exemplos-códigos/index.ts
+++ Eduzz Fullstack Developer/Introdução ao TypeScript/Exemplos-códigos/index.ts
@@ -1,3 +1,11 @@
-console.log('Teste do Typescript no console');
+/*console.log('Teste do Typescript no console');
 document.write('<h1>Testando o Typescript na página html</h1>');
-alert('Testando o Typescript no Alert');
+alert('Testando o Typescript no Alert');*/
+
+//soma(a : number, b: number)
+//soma(4,4);
+
+function soma(a, b){
+    return a + b;
+}
+
