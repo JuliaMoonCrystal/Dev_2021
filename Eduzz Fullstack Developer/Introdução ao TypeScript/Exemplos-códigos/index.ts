d495f64360dce8716fdb587ef648fa818fc8d348
Julia <jufarias20002@gmail.com>
2021-11-22 08:54:33 -0300

    Trabalhando com types e interfaces com Typescript

--- Eduzz Fullstack Developer/Introdução ao TypeScript/Exemplos-códigos/index.ts
+++ Eduzz Fullstack Developer/Introdução ao TypeScript/Exemplos-códigos/index.ts
@@ -1,6 +1,6 @@
 /*console.log('Teste do Typescript no console');
 document.write('<h1>Testando o Typescript na página html</h1>');
-alert('Testando o Typescript no Alert');*/
+alert('Testando o Typescript no Alert');
 
 //soma(a : number, b: number)
 //soma(4,4);
@@ -8,4 +8,44 @@ alert('Testando o Typescript no Alert');*/
 function soma(a, b){
     return a + b;
 }
+*/
 
+//types
+
+interface Canino extends Animal{
+    porte : 'pequeno' | 'médio' |' grande'
+}
+
+type Domestico =Felinos |Canino;
+
+
+//interfaces
+
+interface Animal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    som(som: string): void;
+    domestico : boolean;
+}
+
+interface Felinos extends Animal {
+    visaoNoturna: boolean;
+}
+
+const animal: Animal = {  /* --> Domestico */
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    som: (som) => (`${som}`),
+    domestico : false
+}
+
+const felino: Felinos = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    som: (som) => (`${som}`),
+    domestico : false
+}
+
+
+console.log('O animal' + animal.nome + ' é do tipo ' + animal.tipo);
