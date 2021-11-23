1aca3b4877172fedd4d1f7718d7d6c95a2975ce4
Julia <jufarias20002@gmail.com>
2021-11-22 22:05:20 -0300

    Trabalhando com input

--- Eduzz Fullstack Developer/Introdução ao TypeScript/Exemplos-códigos/index.ts
+++ Eduzz Fullstack Developer/Introdução ao TypeScript/Exemplos-códigos/index.ts
@@ -52,3 +52,48 @@ input.addEventListener('input', (event) => {
     const i = event.currentTarget as HTMLInputElement;
     console.log(i.value);
 })
+
+//Generic Types
+function adicionar(array: any[], valor: any): any { //vai conferir os tipos de acordo com oo que o usuário colocarn
+    return array.map(item => item + valor);
+}
+
+adicionar([1, 2, 3, 4, 5], 1);
+
+interface Usuario {
+    id: number,
+    email: string
+}
+
+interface Adimonistrador extends Usuario {
+    cargo: 'supervisor' | 'coordernador' | 'normal';
+}
+
+function redirecionar(usuario: Usuario | Adimonistrador) {
+    if ('cargo' in usuario) {
+        ///área de adiministração
+    } else {
+        //redirecionar para a área do usuário
+    }
+}
+
+function redirecionar2(usuario: Adimonistrador) {
+    if (usuario.cargo) {
+        ///redireciona cargo
+    }
+    else {
+        //redireciona para a área normal
+    }
+}
+
+interface Cachorro {
+    nome: string,
+    idade: number,
+    localFavorito?: string;
+}
+
+const myDog : Cachorro ={
+    nome :'Artemis',
+    idade :12,
+    localFavorito :'Praça'
+}
