61fc8ca7cba83c7f2543e67f59327a4355386989
Julia <jufarias20002@gmail.com>
2021-12-07 15:18:44 -0300

    Exercicio com Sobrecarga

---
+++ 1#java/Entendendo Metodos Java/ExerciciosMetodos/src/Exercicio2/Principal.java
@@ -0,0 +1,14 @@
+package Exercicio2;
+
+public class Principal {
+	 public static void main(String[] args) {
+
+	        // Quadrilátero
+	        System.out.println("Exercício quadrilátero");
+	        Quadrilatero.area(3);
+	        Quadrilatero.area(5d,5d);
+	        Quadrilatero.area(7,8,9);
+	        Quadrilatero.area(5f,5f);
+
+	    }
+}
