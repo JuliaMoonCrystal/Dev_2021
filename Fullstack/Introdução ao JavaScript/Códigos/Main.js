d3cae61f238c7b565d8d27c92345c6a1fc28efd0
Julia <jufarias20002@gmail.com>
2021-12-17 14:13:10 -0300

    Definindo o projeto

---
+++ Fullstack/Introdução ao JavaScript/Códigos/Main.js
@@ -0,0 +1,13 @@
+//var y =0; --> comentario no javascript
+/*var x=0;
+var f=9;  --> comentario de várias linhas*/
+
+var a=9;
+var b=8;
+
+var valor= soma(a,b);
+console.log(valor);
+
+function soma(a,b) {
+    return a+b;
+}
