9cfdc8d198966bb05a8d411038042bc64b2a384c
Julia <jufarias20002@gmail.com>
2021-12-19 19:58:01 -0300

    Contador

--- Fullstack/Introdução ao JavaScript/Códigos/Main.js
+++ Fullstack/Introdução ao JavaScript/Códigos/Main.js
@@ -2,12 +2,39 @@
 /*var x=0;
 var f=9;  --> comentario de várias linhas*/
 
-var a=9;
-var b=8;
+/*var a = 9;
+var b = 8;
 
-var valor= soma(a,b);
+var valor = soma(a, b);
 console.log(valor);
 
-function soma(a,b) {
-    return a+b;
+function soma(a, b) {
+    return a + b;
+}
+*/
+
+function troca() {
+    const frase = document.getElementById('h1');
+    console.log(frase);
+
+    h1.style.color = 'red';
+}
+
+function volta() {
+    const frase = document.getElementById('h1');
+
+    h1.style.color = 'black';
+}
+
+var num= document.getElementById('currentNumber');
+var cont=0;
+
+function increment() {
+    cont+=1;
+    num.innerHTML= cont;
+}
+
+function decrement() {
+    cont-=1;
+    num.innerHTML= cont;
 }
